a1826f1bac656d7945b233101a26bf3f563ca23c
tbreuss <thomasbreuss@gmx.ch>
2018-12-02 19:25:42 +0100

    Add dist config files

---
+++ config/db.dist.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=MY_HOST;dbname=MY_DBNAME',
+    'username' => 'MY_USERNAME',
+    'password' => 'MY_PASSWORD',
+    'charset' => 'utf8'
+];
---
+++ config/params.dist.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'jwt.private_key' => 'MY_PRIVATE_JWT_KEY'
+];
